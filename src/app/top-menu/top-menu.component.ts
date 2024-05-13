1783caf7608d52ef4f73f83b07332e1aed96cd1e
Edward Tanguay (windows) <edward@tanguay.info>
2024-05-14 00:16:30 +0200

    basic menu works with live-link

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-top-menu',
   standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './top-menu.component.html',
   styleUrl: './top-menu.component.scss'
 })

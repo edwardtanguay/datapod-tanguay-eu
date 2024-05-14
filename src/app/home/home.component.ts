2861aa719e8fab55a0fdee287c4b618e56f0d541
Edward Tanguay (windows) <edward@tanguay.info>
2024-05-14 23:03:45 +0200

    dummy tasks being read

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import _tasks from "../../assets/data/tasks.json";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: "app-home",
+	standalone: true,
+	imports: [],
+	templateUrl: "./home.component.html",
+	styleUrl: "./home.component.scss",
 })
 export class HomeComponent {
-
+	title = "test111";
+  tasks = _tasks;
 }

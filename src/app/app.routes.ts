fc6bf6ca2892e1218da24e40e1646a8e69f116da
Edward Tanguay (windows) <edward@tanguay.info>
2024-05-18 16:01:27 +0200

    create stub tasks page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'tasks', component: TasksComponent }
 ];
 
 

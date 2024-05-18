7e237a5c0eb3aaa81bd9f87ff2b90ec8a80707a6
Edward Tanguay (windows) <edward@tanguay.info>
2024-05-18 16:05:19 +0200

    tasks/home switched

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,11 @@
 import { Component } from "@angular/core";
-import { CommonModule } from '@angular/common';
-import _tasks from "../../data/tasks.json";
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [CommonModule],
+	imports: [],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.scss",
 })
 export class HomeComponent {
-	title = "test111";
-  tasks = _tasks.sort((a,b) => a.rank < b.rank ? 1 : -1);
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import _tasks from "../../data/tasks.json";
 
 @Component({
-  selector: 'app-tasks',
-  standalone: true,
-  imports: [],
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+	selector: "app-tasks",
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: "./tasks.component.html",
+	styleUrl: "./tasks.component.scss",
 })
 export class TasksComponent {
-
+	tasks = _tasks.sort((a, b) => (a.rank < b.rank ? 1 : -1));
 }

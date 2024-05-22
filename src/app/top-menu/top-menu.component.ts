761d15b62bfc0f1bfee4864647d5bbc14221b876
Edward Tanguay (windows) <edward@tanguay.info>
2024-05-22 23:24:36 +0200

    menu in array

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet, RouterLink, RouterLinkActive } from "@angular/router";
+import _pageItems from "../../data/pageItems.json";
 
 @Component({
-  selector: 'app-top-menu',
-  standalone: true,
-    imports: [RouterOutlet, RouterLink, RouterLinkActive],
-  templateUrl: './top-menu.component.html',
-  styleUrl: './top-menu.component.scss'
+	selector: "app-top-menu",
+	standalone: true,
+	imports: [RouterOutlet, RouterLink, RouterLinkActive],
+	templateUrl: "./top-menu.component.html",
+	styleUrl: "./top-menu.component.scss",
 })
 export class TopMenuComponent {
-
+  pageItems = structuredClone(_pageItems);
 }

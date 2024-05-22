b6cc34cdca6e3b6c43b454c2111cc6afcbaaf0da
Edward Tanguay (windows) <edward@tanguay.info>
2024-05-22 23:35:24 +0200

    base starters page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { StartersComponent } from './starters/starters.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'starters', component: StartersComponent },
   { path: 'about', component: AboutComponent },
   { path: 'tasks', component: TasksComponent }
 ];
